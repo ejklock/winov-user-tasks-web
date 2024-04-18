cff349c23d2228cf3042255d75a157ad0eadb350
Evaldo Klock <neto.nemesis@gmail.com>
2024-04-18 15:20:59 -0400

    chore: remove console log

--- src/utils/transformers.ts
+++ src/utils/transformers.ts
@@ -26,16 +26,14 @@ export const convertApiValidationErrorsToFormErrors = (
     for (const field in errors) {
       errorsToSet[field] = errors[field][0]
     }
-    console.log(errorsToSet)
 
     return errorsToSet
   } else if (apiResponse?.message) {
-    console.log(apiResponse)
     errorsToSet['message'] = apiResponse.message
-    console.log(errorsToSet)
+
     return errorsToSet
   } else {
-    return {} // Return an empty object if apiResponse does not have an errors property
+    return {}
   }
 }
 
